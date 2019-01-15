21f6ebb3e4899c7b2c71e5bd56dd40ed5695157b
alongkorn <alongkorn.du.57@ubu.ac.th>
2019-01-15 17:50:11 +0700

    upload file project

---
+++ project/index.js
@@ -0,0 +1,25 @@
+{/* <script>
+  export default {
+    data: () => ({
+      icons: [
+        'fab fa-facebook',
+        'fab fa-twitter',
+        'fab fa-google-plus',
+        'fab fa-linkedin',
+        'fab fa-instagram'
+      ]
+    })
+  }
+</script> */}
+
+export default {
+    data: () => ({
+      icons: [
+        'fab fa-facebook',
+        'fab fa-twitter',
+        'fab fa-google-plus',
+        'fab fa-linkedin',
+        'fab fa-instagram'
+      ]
+    })
+  }
---
+++ project/register.js
@@ -0,0 +1,9 @@
+  export default {
+    data () {
+      return {
+        checkbox: true,
+        radioGroup: 1,
+        switch1: true
+      }
+    }
+  }
